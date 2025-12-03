2a9387ed0803ec3ccfb455a14818ef45ad3d8c45
Ducnguyn97 <nguyenduc8197@gmail.com>
2025-12-03 19:19:11 +0700

    first commit

---
+++ src/main/java/vn/codegym/BE_BookOnline/BeBookOnlineApplication.java
@@ -0,0 +1,13 @@
+package vn.codegym.BE_BookOnline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeBookOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeBookOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/codegym/BE_BookOnline/BeBookOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.codegym.BE_BookOnline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeBookOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
